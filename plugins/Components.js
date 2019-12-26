2d08e6ceb9c6ab0c9cae7d8ae680125f35ec96ff
Cedric Willekens <c.s.willekens@student.tudelft.nl>
2019-12-26 16:12:19 -0300

    See if this works?

--- plugins/Components.js
+++ plugins/Components.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCoffee,
-  faChild,
-  faCircle,
-  faArchive,
-  faInfoCircle
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faComment,
-  faFilePowerpoint
-} from '@fortawesome/free-regular-svg-icons'
-import { faTwitter, faAdversal } from '@fortawesome/free-brands-svg-icons'
-import {
-  FontAwesomeIcon,
-  FontAwesomeLayers,
-  FontAwesomeLayersText
-} from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import {
+//   faCoffee,
+//   faChild,
+//   faCircle,
+//   faArchive,
+//   faInfoCircle
+// } from '@fortawesome/free-solid-svg-icons'
+// import {
+//   faComment,
+//   faFilePowerpoint
+// } from '@fortawesome/free-regular-svg-icons'
+// import { faTwitter, faAdversal } from '@fortawesome/free-brands-svg-icons'
+// import {
+//   FontAwesomeIcon,
+//   FontAwesomeLayers,
+//   FontAwesomeLayersText
+// } from '@fortawesome/vue-fontawesome'
 import CategoriesGrid from '../components/grids/CategoriesGrid'
 import DisqusComments from '../components/DisqusComments'
 import IntersectionObserver from '../components/IntersectionObserver'
@@ -30,21 +30,21 @@ import SiteLogo from '../components/SiteLogo'
 import SiteNav from '../components/SiteNav'
 import ResourceGrid from '~/components/grids/ResourceGrid'
 
-library.add(
-  faCoffee,
-  faChild,
-  faCircle,
-  faArchive,
-  faComment,
-  faTwitter,
-  faInfoCircle,
-  faAdversal,
-  faFilePowerpoint
-)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.component('font-awesome-layers', FontAwesomeLayers)
-Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
+// library.add(
+//   faCoffee,
+//   faChild,
+//   faCircle,
+//   faArchive,
+//   faComment,
+//   faTwitter,
+//   faInfoCircle,
+//   faAdversal,
+//   faFilePowerpoint
+// )
+//
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.component('font-awesome-layers', FontAwesomeLayers)
+// Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.component(CategoriesGrid.name, CategoriesGrid)
 Vue.component(DisqusComments.name, DisqusComments)
